f571fd462b9321a01923e88f4bb7acfa18cf1b5a
Agustin Ranieri <aguseranieri@gmail.com>
2024-02-11 01:27:35 -0300

    Inject JPAQL

--- src/test/java/io/github/flbulgarelli/jpa/extras/SampleUser.java
+++ src/test/java/io/github/flbulgarelli/jpa/extras/SampleUser.java
@@ -11,8 +11,13 @@ public class SampleUser {
   @Id
   @GeneratedValue
   private Long id;
+  private String name;
 
   public Long getId() {
     return id;
+  }
+
+  public String getName() {
+    return name;
   }
 }
